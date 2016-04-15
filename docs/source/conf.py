8c7c91ce5cc2d47397d30eb27b5007ab4b9265cc
Duncan McDowell <duncan@blitzen.com>
2016-04-15 12:22:34 -0400

    move this to see if it makes a difference

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,6 @@
 import sys
 import os
 import shlex
-from recommonmark.parser import CommonMarkParser
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -35,15 +34,6 @@ extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-
-# source_parser for allowing markdown
-source_parsers = {
-    '.md': CommonMarkParser, 
-}
-
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
@@ -288,3 +278,13 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+from recommonmark.parser import CommonMarkParser
+
+# The suffix of source filenames.
+source_suffix = ['.rst', '.md']
+
+source_parsers = {
+  '.md': CommonMarkParser,
+}
+
